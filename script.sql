c8ea5c333459d697f9c687df591428ca9bd77914
Kemovizor <kemal.maric@edu.fit.ba>
2019-02-20 19:15:48 +0100

    written sql code for creating tables

--- script.sql
+++ script.sql
@@ -40,3 +40,37 @@ compliment_funny int,
 compliment_writer int,
 compliment_photos int
 );
+
+create table Reviews(
+review_id varchar(22) not null primary key,
+user_id varchar(22) references Users(user_id),
+business_id varchar(22) references Businesses(business_id),
+stars float,
+useful int,
+funny int,
+cool int,
+text varchar,
+date varchar
+);
+    
+create table Checkins(
+business_id varchar(22) not null references Businesses(business_id),
+date varchar,
+primary key(business_id)
+);
+
+create table Tips(
+user_id varchar(22) not null references Users(user_id),
+business_id varchar(22) not null references Businesses(business_id),
+text varchar,
+date varchar,
+compliment_count int
+--primary key(user_id, business_id)
+);
+
+create table Photos(    
+caption varchar,
+photo_id varchar(22) not null primary key,
+business_id varchar(22) null references Businesses(business_id),
+label varchar
+);
